0398454a2de43c920766cffe58f188aed6062493
Jonas Jelten <jj@sft.lol>
2024-03-21 16:05:55 +0100

    emacs: enable collab and ein modules

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -88,13 +88,13 @@
        :tools
        ansible
        biblio            ; Writes a PhD for you (citation needed)
+       collab            ; buffers with friends
        debugger          ; FIXME stepping through code, to help you add bugs
        ;direnv
        ;docker
        editorconfig      ; let someone else argue about tabs vs spaces
-       ;;ein               ; tame Jupyter notebooks with emacs
+       ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
        (lsp +eglot)               ; M-x vscode
        magit             ; a git porcelain for Emacs
@@ -110,7 +110,7 @@
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
-       (:if IS-MAC macos)  ; improve compatibility with macOS
+       (:if (featurep :system 'macos) macos)  ; improve compatibility with macOS
        tty               ; improve the terminal Emacs experience
 
        :lang
