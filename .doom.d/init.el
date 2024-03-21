8f8eb9431aabed292b6298b6162613b9b8e10183
Jonas Jelten <jj@sft.lol>
2024-03-21 16:05:39 +0100

    emacs: use corfu instead of company

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -21,7 +21,8 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       (company +childframe) ; the ultimate code completion backend
+       ;;(company +childframe) ; the ultimate code completion backend
+       (corfu +orderless +dabbrev +icons)  ; complete with cap(f), cape and a flying feather!
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
