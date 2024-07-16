4c5090111736dcb88eacaf63493ccc722c91ea98
Jonas Jelten <jj@sft.lol>
2024-07-16 17:08:25 +0200

    emacs: enable pass password-manager module

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -98,7 +98,7 @@
        (lsp +eglot)               ; M-x vscode
        magit             ; a git porcelain for Emacs
        make              ; run make tasks from Emacs
-       ;;pass              ; password manager for nerds
+       pass              ; password manager for nerds
        pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;terraform         ; infrastructure as code
