bf0ee9dabd854a35edec851721f4801cd290cc37
Jonas Jelten <jj@sft.lol>
2025-06-25 10:47:02 +0200

    emacs: use default dap debugger and disable lsp headerline diagnostics

--- .doom.d/lsp.el
+++ .doom.d/lsp.el
@@ -6,7 +6,6 @@
         lsp-enable-imenu nil
         lsp-enable-xref t
         lsp-headerline-breadcrumb-enable t
-        lsp-headerline-breadcrumb-enable-diagnostics nil
         lsp-enable-on-type-formatting nil  ; using t funnily changes screen content whenever lsp thinks it can do "formatting"
         lsp-enable-file-watchers nil       ; lsp server can do inotify itself, but that may slow emacs down (https://github.com/MaskRay/ccls/issues/354)
         lsp-eldoc-enable-hover t           ; display info about thing at cursor in minibuffer
@@ -16,7 +15,7 @@
         lsp-signature-doc-lines 1
         lsp-enable-snippet t
         lsp-warn-no-matched-clients nil    ; don't warn if there's no lsp client.
-        dap-python-debugger 'debugpy)
+        )
 
   ;; update treemacs folders from lsp
   (after! lsp-treemacs
